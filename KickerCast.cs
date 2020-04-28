c05a5b45846ab227cf7284f43bc6f2a21a6db000
JesseBoe <JesseBoe@gmail.com>
2020-04-28 11:19:23 -0700

    Added support for multiple kicker casts

--- KickerCast.cs
+++ KickerCast.cs
@@ -11,6 +11,8 @@ using UnityEngine;
 //A kicker cast is a spell that behaves differently depending on how much mana the player has. Generally looks like this. Cast mini thunder for zero mana,
 //but if the player has 3 mana cast thunderstorm instead.
 //EX: <Params KickerCast="true" KickerManaCost="4" KickerSpell="StormThunder"></Params>
+//Kicker cast also supports multiple kickers
+//EX: <Params KickerCast="true" KickerManaCost="1,4" KickerSpell="Thunder,StormThunder"></Params>
 
 [HarmonyPatch(typeof(Player))]
 [HarmonyPatch("CastSpell")]
@@ -30,15 +32,21 @@ class MoreLuaPower_Kickercast
                 Debug.Log("ERROR: Spell has KickerCast, but not KickerSpell");
                 return true;
             }
-            //Does the player have the mana for the kicker cost or manaOverride cost
-            if (__instance.duelDisk.currentMana >= (manaOverride < 0 ? Int32.Parse(pd["KickerManaCost"]) : manaOverride)) {
 
-                SpellObject Kicker = S.I.deCtrl.CreateSpellBase(pd["KickerSpell"], __instance);
-                Kicker.PlayerCast();
-                var manaCost = (manaOverride < 0 ? Int32.Parse(pd["KickerManaCost"]) : manaOverride);
-                __instance.duelDisk.currentMana -= (float)manaCost;
-                __instance.duelDisk.LaunchSlot(slotNum, false, null);
-                return false;
+            List<string> SpellNames = pd["KickerSpell"].Split(',').ToList();
+            List<string> ManaCosts = pd["KickerManaCost"].Split(',').ToList();
+
+            for (int i = SpellNames.Count-1; i >= 0; i--)
+            {
+                if (__instance.duelDisk.currentMana >= (manaOverride < 0 ? Int32.Parse(ManaCosts[i]) : manaOverride))
+                {
+                    SpellObject Kicker = S.I.deCtrl.CreateSpellBase(SpellNames[i], __instance);
+                    Kicker.PlayerCast();
+                    var manaCost = (manaOverride < 0 ? Int32.Parse(ManaCosts[i]) : manaOverride);
+                    __instance.duelDisk.currentMana -= (float)manaCost;
+                    __instance.duelDisk.LaunchSlot(slotNum, false, null);
+                    return false;
+                }
             }
         }
         return true;
