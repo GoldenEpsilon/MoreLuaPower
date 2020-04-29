ff2401d6e5fa4040c2e519da5c1b33958b260b84
JesseBoe <JesseBoe@gmail.com>
2020-04-29 13:10:43 -0700

    Messing with boss music

---
+++ PlayCustomBattleMusic.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using I2.Loc;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[HarmonyPatch(typeof(BC))]
+[HarmonyPatch("StartBattle")]
+class MoreLuaPower_PlayCustomBattleMusic
+{
+    static void Postfix(ref SpawnCtrl __instance)
+    {
+        var AllAudioClips = Traverse.Create(S.I.itemMan).Field("allAudioClips").GetValue<Dictionary<String, AudioClip>>();
+        S.I.muCtrl.Play(AllAudioClips["YoumuBossTheme"], true);
+    }
+}
