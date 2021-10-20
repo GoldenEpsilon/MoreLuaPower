9ec79bbc9d128bfea1714a8751e5ef7d9eb6eca8
DecoyDoge <spamninja4@gmail.com>
2021-10-20 16:24:28 -0400

    Added drop checks for boss and miniboss rewards

--- Additions/CustomDropChecks.cs
+++ Additions/CustomDropChecks.cs
@@ -4,8 +4,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class LuaPowerDropChecks
+static class LuaPowerDropChecks
 {
 	public static void AddDropCheck(string name, string funcName) {
 		S.I.mainCtrl.StartCoroutine(_AddDropCheck(name, funcName));
@@ -31,4 +32,53 @@ static class MoreLuaPower_CustomDropChecks
 			}
 		}
 	}
+}
+
+[HarmonyPatch(typeof(PostCtrl))]
+[HarmonyPatch("StartOptions")]
+static class MoreLuaPower_CustomDropChecks_Boss
+{
+	static void Prefix(PostCtrl __instance, ref List<ItemObject> finalOptions, RewardType rewardType, int siblingIndex = -1)
+    {
+		//Regenerate boss reward
+		if(S.I.runCtrl.currentZoneDot.type == ZoneType.Boss)
+        {
+			for (int i = finalOptions.Count - 1; i >= 0; i--)
+			{
+				if (LuaPowerData.dropChecks.ContainsKey(finalOptions[i].itemID))
+				{
+					Script mainscr = Traverse.Create(Traverse.Create<EffectActions>().Field("_Instance").GetValue<EffectActions>()).Field("myLuaScript").GetValue<Script>();
+					if (!mainscr.Call(mainscr.Globals[LuaPowerData.dropChecks[finalOptions[i].itemID]], new object[] { finalOptions[i] }).Boolean)
+					{
+						string removedID = finalOptions[i].itemID;
+						finalOptions.Remove(finalOptions[i]);
+						List<ArtifactObject> source = S.I.itemMan.bossRewards;
+						if (S.I.batCtrl.currentPlayer.health.current == S.I.batCtrl.currentPlayer.health.max)
+							source = source.Where<ArtifactObject>((Func<ArtifactObject, bool>)(t => !t.tags.Contains(Tag.Heal))).Where(a => a.itemID != removedID).ToList<ArtifactObject>();
+						finalOptions.Add((ItemObject)source[S.I.runCtrl.NextPsuedoRand(0, source.Count)].Clone());
+					}
+				}
+			}
+		}
+		//Regenerate miniboss reward
+		if(S.I.runCtrl.currentZoneDot.type == ZoneType.Miniboss)
+        {
+			for (int i = finalOptions.Count - 1; i >= 0; i--)
+			{
+				if (LuaPowerData.dropChecks.ContainsKey(finalOptions[i].itemID))
+				{
+					Script mainscr = Traverse.Create(Traverse.Create<EffectActions>().Field("_Instance").GetValue<EffectActions>()).Field("myLuaScript").GetValue<Script>();
+					if (!mainscr.Call(mainscr.Globals[LuaPowerData.dropChecks[finalOptions[i].itemID]], new object[] { finalOptions[i] }).Boolean)
+					{
+						string removedID = finalOptions[i].itemID;
+						finalOptions.Remove(finalOptions[i]);
+						List<ArtifactObject> source = S.I.itemMan.minibossRewards;
+						if (S.I.batCtrl.currentPlayer.health.current == S.I.batCtrl.currentPlayer.health.max)
+							source = source.Where<ArtifactObject>((Func<ArtifactObject, bool>)(t => !t.tags.Contains(Tag.Heal))).Where(a => a.itemID != removedID).ToList<ArtifactObject>();
+						finalOptions.Add((ItemObject)source[S.I.runCtrl.NextPsuedoRand(0, source.Count)].Clone());
+					}
+				}
+			}
+		}
+    }
 }
