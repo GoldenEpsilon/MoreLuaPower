b936affec0e437a0083a66eaa05c43ee52f334b1
Diane Ngo <mrperson7895@gmail.com>
2022-02-18 11:45:10 -0800

    add random upgrade function

--- Additions/CustomUpgrades.cs
+++ Additions/CustomUpgrades.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using MoonSharp.Interpreter;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 using UnityEngine;
@@ -30,6 +31,20 @@ static class LuaPowerUpgrades
 		XmlReader reader = XmlReader.Create(new StringReader("<Spell itemID=\""+ spellObj.itemID +"\">" + XML + "</Spell>"));
 		if (reader.ReadToDescendant("Spell")) {
 			spellObj.ReadXmlPrototype(reader);
+		}
+	}
+
+	static public void ApplyRandomUpgrade(SpellObject spellObj) {
+		int count = spellObj.enhancements.Count;
+		List<int> intList = Utils.RandomList(Enum.GetNames(typeof(Enhancement)).Length, true);
+		for (int i = 0; i < intList.Count; ++i)
+		{
+			S.I.poCtrl.EnhanceSpell(spellObj, (Enhancement)intList[i]);
+			if (spellObj.enhancements.Count > count)
+			{
+				count = spellObj.enhancements.Count;
+				break;
+			}
 		}
 	}
 }
