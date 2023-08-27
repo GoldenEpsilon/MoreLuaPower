8e91e2d91b688be167af097d5d97c8ef5e40a475
Hanna <115431093+HanaOcha@users.noreply.github.com>
2023-08-27 19:23:45 -0400

    Updated CustomSettings.cs with Folders and UI Fixes

--- Additions/CustomSettings.cs
+++ Additions/CustomSettings.cs
@@ -1,23 +1,30 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-
-enum SettingType { 
+enum SettingType {
     Toggle,
-	Rotation,
-	Slider
+    Rotation,
+    Slider,
+    Folder,
+
+    Return // Used for folder return button
 }
 
 class MPLSetting {
 	public string name;
 	public SettingType type;
 	public List<string> values;
+
 	public int activeValue = 0;
 	public int defaultValue = 0;
 	public float defaultSliderValue = 0;
+
+	public bool inFolder = false;
+
 	public GameObject settingobj;
 	public Transform control;
 }
@@ -29,7 +36,12 @@ static class MPLCustomSettings
 	public static int settingsPage = 0;
 	public static Transform previousPage;
 	public static Transform nextPage;
-	public static bool GetSettingToggle(string name) {
+
+    	internal static bool folderActive = false;
+    	internal static string currentFolder = "FolderSetup|Outside";
+    	internal static string folderReturnKey = "FolderSetup|ReturnKey";
+
+    	public static bool GetSettingToggle(string name) {
 		if (!settings.ContainsKey(name)) {
 			MPLog.Log("Setting " + name + " does not exist!", LogLevel.Major);
 			return false;
@@ -62,7 +74,29 @@ static class MPLCustomSettings
 		}
 		return PlayerPrefs.GetFloat(name);
 	}
-	public static void AddSettingToggle(string name, bool defaultval) {
+    public static List<string> GetSettingFolder(string name)
+    {
+        if (name == null)
+        {
+            MPLog.Log("Null setting name given!", LogLevel.Major);
+            return null;
+        }
+        if (!settings.ContainsKey(name))
+        {
+            MPLog.Log("Setting " + name + " does not exist!", LogLevel.Major);
+            return null;
+        }
+        if (settings[name].type != SettingType.Folder)
+        {
+            MPLog.Log("Setting " + name + " is a " + settings[name].type.ToString() + ", when a Folder was asked for.", LogLevel.Major);
+            return null;
+        }
+
+        UpdateFolders();
+        return settings[name].values;
+    }
+
+    public static void AddSettingToggle(string name, bool defaultval) {
 		if (!settings.ContainsKey(name)) {
 			MPLSetting setting = new MPLSetting();
 			setting.name = name;
@@ -96,7 +130,33 @@ static class MPLCustomSettings
 			MPLog.Log("Setting " + name + " was not added as it was already an initialized setting", LogLevel.Minor);
 		}
 	}
-	public static void NextSettingsPage() {
+    public static void AddSettingFolder(string name)
+    {
+        if (name == null)
+        {
+            MPLog.Log("Null setting name given!", LogLevel.Minor);
+            return;
+        }
+        if (settings.ContainsKey(name))
+        {
+            MPLog.Log("Folder '" + name + "' is already an initialized setting", LogLevel.Minor);
+            return;
+        }
+
+        MPLSetting setting = new MPLSetting();
+        setting.name = name;
+        setting.values = new List<string>();
+        setting.type = SettingType.Folder;
+
+        List<KeyValuePair<string, MPLSetting>> sorted = settings.ToList();
+        int folderCount = 0;
+        foreach (MPLSetting value in settings.Values) { if (value.type == SettingType.Folder) { folderCount++; } }
+        sorted.Insert(folderCount, new KeyValuePair<string, MPLSetting>(name, setting));
+        settings.Clear();
+        foreach (KeyValuePair<string, MPLSetting> pair in sorted) { settings.Add(pair.Key, pair.Value); }
+    }
+
+    public static void NextSettingsPage() {
 		if (settings.Count > settingsPage * 18 + 2) {
 			settingsPage++;
 			UpdateSettingsPage();
@@ -109,8 +169,57 @@ static class MPLCustomSettings
 		}
 	}
 	public static void UpdateSettingsPage() {
-		int controlNum = 0;
-		foreach (MPLSetting setting in settings.Values) {
+        if (settings.ContainsKey(folderReturnKey)) { settings[folderReturnKey].settingobj.SetActive(false); }
+
+        foreach (KeyValuePair<string, MPLSetting> pair in settings)
+        {
+            pair.Value.control.GetComponent<TextMeshProUGUI>().color= U.I.GetColor(UIColor.White);
+        }
+        nextPage.GetChild(0).GetComponent<TextMeshProUGUI>().color = U.I.GetColor(UIColor.White);
+        previousPage.GetChild(0).GetComponent<TextMeshProUGUI>().color = U.I.GetColor(UIColor.White);
+
+        int controlNum = 0;
+
+        if (folderActive)
+		{
+			foreach (KeyValuePair<string, MPLSetting> pair in settings)
+			{
+				MPLSetting option = pair.Value;
+                option.settingobj.SetActive(false);
+                if (pair.Key.StartsWith(currentFolder + "/") & pair.Value.inFolder)
+				{
+                    if (controlNum < 18)
+					{
+						controlNum++;
+                        option.settingobj.SetActive(true);
+                    }
+                }
+            }
+
+			settings[folderReturnKey].settingobj.SetActive(true);
+
+            previousPage.gameObject.SetActive(false);
+            nextPage.gameObject.SetActive(false);
+
+            foreach (MPLSetting option in settings.Values)
+			{
+                if (option.settingobj.activeSelf & S.I.optCtrl.content.activeSelf) { S.I.optCtrl.btnCtrl.SetFocus(option.settingobj); break; }
+			}
+
+            return;
+		}
+
+        List<MPLSetting> settingList = new List<MPLSetting>();
+        foreach (KeyValuePair<string, MPLSetting> pair in settings)
+        {
+            if (!pair.Value.inFolder & pair.Key != folderReturnKey)
+            {
+                settingList.Add(pair.Value);
+            }
+            else { pair.Value.settingobj.SetActive(false); }
+        }
+
+        foreach (MPLSetting setting in settingList) {
 			controlNum++;
 			if (controlNum < settingsPage * 18 + 2 && settingsPage != 0) {
 				setting.settingobj.SetActive(false);
@@ -120,16 +229,68 @@ static class MPLCustomSettings
 				setting.settingobj.SetActive(false);
 			}
 		}
-		nextPage.gameObject.SetActive(false);
+
+		if (currentFolder != "FolderSetup|Outside")
+		{
+			if (S.I.optCtrl.content)
+			{
+				S.I.optCtrl.btnCtrl.SetFocus(settings[currentFolder].settingobj);
+                currentFolder = "FolderSetup|Outside";
+			}
+        }
+		else
+		{
+			foreach (MPLSetting option in settings.Values)
+			{
+				if (option.settingobj.activeSelf & S.I.optCtrl.content.activeSelf) { S.I.optCtrl.btnCtrl.SetFocus(option.settingobj); break; }
+			}
+		}
+
+        nextPage.gameObject.SetActive(false);
 		if (settingsPage == 0) {
 			previousPage.gameObject.SetActive(false);
 		} else {
 			previousPage.gameObject.SetActive(true);
 		}
-		if (settings.Count > (settingsPage + 1) * 18 + 2) {
+		if (settingList.Count > (settingsPage + 1) * 18 + 2) {
 			nextPage.gameObject.SetActive(true);
 		} else {
 			nextPage.gameObject.SetActive(false);
+		}
+	}
+	internal static void UpdateFolders()
+	{
+		foreach (MPLSetting setting in settings.Values)
+		{
+			if (setting.type == SettingType.Folder) { setting.values.Clear(); }
+		}
+
+		foreach (KeyValuePair<string, MPLSetting> pair in settings)
+		{
+            if (pair.Key.Contains("/")) 
+			{
+                string folder = pair.Key.Substring(0, pair.Key.IndexOf("/"));
+                if (settings.ContainsKey(folder))
+                {
+                    if (pair.Value.name.StartsWith(folder + "/"))
+					{
+                        pair.Value.name = pair.Value.name.Substring(pair.Value.name.IndexOf("/") + 1);
+                    }
+                    pair.Value.inFolder = true;
+
+                    if (!settings[folder].values.Contains(pair.Value.name))
+                    {
+                        settings[folder].values.Add(pair.Value.name);
+                    }
+                }
+            }
+        }
+
+		if (!settings.ContainsKey(folderReturnKey))
+		{
+			MPLSetting returnBtn = new MPLSetting();
+			returnBtn.type = SettingType.Return;
+			settings.Add(folderReturnKey, returnBtn);
 		}
 	}
 }
@@ -140,9 +301,12 @@ class SettingsPatch
 {
 	static void Prefix(OptionCtrl __instance) {
 		if (MPLCustomSettings.SettingsSetUp == false) {
-			var button = Object.Instantiate(__instance.navButtonGrid.GetChild(1), __instance.navButtonGrid);
+            MPLCustomSettings.UpdateFolders();
+
+            var button = Object.Instantiate(__instance.navButtonGrid.GetChild(1), __instance.navButtonGrid);
 			button.GetComponent<UIButton>().tmpText.text = "MODS";
 			button.GetChild(0).GetComponent<I2.Loc.Localize>().Term = "MODS";
+			if (button.parent.childCount >= 4) { button.SetSiblingIndex(3); }
 
 			var modSettingsMenu = Object.Instantiate(__instance.settingsPane.gameObject);
 			modSettingsMenu.transform.position = __instance.settingsPane.transform.position;
@@ -170,7 +334,9 @@ class SettingsPatch
 			{
 				EventTrigger.Entry entry = new EventTrigger.Entry();
 				entry.eventID = EventTriggerType.PointerClick;
-				entry.callback.AddListener((data) => { S.I.optCtrl.OpenPanel(modSettingsMenu.GetComponent<NavPanel>()); });
+				entry.callback.AddListener((data) => {
+					S.I.optCtrl.OpenPanel(modSettingsMenu.GetComponent<NavPanel>());
+				});
 				EventTrigger trigger = button.gameObject.AddComponent<EventTrigger>();
 				trigger.triggers.Add(entry);
 				trigger.enabled = false;
@@ -193,7 +359,8 @@ class SettingsPatch
 			GameObject PrevButton = Object.Instantiate(navPanelButtons.transform.GetChild(0).gameObject, navPanelButtons.transform);
 			PrevButton.SetActive(true);
 			PrevButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Previous Page";
-			PrevButton.transform.GetChild(0).GetComponent<I2.Loc.Localize>().Term = "Previous Page";
+            PrevButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().alignment = TextAlignmentOptions.Left;
+            PrevButton.transform.GetChild(0).GetComponent<I2.Loc.Localize>().Term = "Previous Page";
 			Object.DestroyImmediate(PrevButton.GetComponent<Button>());
 			{
 				EventTrigger.Entry entry = new EventTrigger.Entry();
@@ -205,7 +372,7 @@ class SettingsPatch
 			}
 			MPLCustomSettings.previousPage = PrevButton.transform;
 
-			foreach (MPLSetting setting in MPLCustomSettings.settings.Values) {
+            foreach (MPLSetting setting in MPLCustomSettings.settings.Values) {
 				switch (setting.type) {
 					case SettingType.Toggle:
 						if (PlayerPrefs.HasKey(setting.name)) {
@@ -271,14 +438,78 @@ class SettingsPatch
 						}
 						PowerMonoBehavior.sliders.Add(setting.settingobj.transform);
 						break;
+					case SettingType.Folder:
+                        setting.settingobj = Object.Instantiate(navPanelButtons.transform.GetChild(0).gameObject, navPanelButtons.transform);
+                        setting.settingobj.SetActive(true);
+                        setting.control = setting.settingobj.transform.GetChild(0);
+                        setting.control.GetComponent<TextMeshProUGUI>().text = setting.name;
+						if (setting.values.Count > 0) 
+						{ 
+							setting.control.GetComponent<TextMeshProUGUI>().text += " [ +" + setting.values.Count.ToString() + " ]"; 
+						}
+						else
+						{
+                            setting.control.GetComponent<TextMeshProUGUI>().text += " []";
+                        }
+
+                        setting.control.GetComponent<TextMeshProUGUI>().fontStyle = FontStyles.Bold;
+                        setting.control.GetComponent<TextMeshProUGUI>().alignment = TextAlignmentOptions.TopLeft;
+
+                        setting.control.GetComponent<I2.Loc.Localize>().Term = "-";
+
+                        Object.DestroyImmediate(setting.settingobj.GetComponent<Button>());
+                        {
+                            EventTrigger.Entry entry = new EventTrigger.Entry();
+                            entry.eventID = EventTriggerType.PointerClick;
+                            entry.callback.AddListener((data) => {
+								MPLCustomSettings.folderActive = true;
+								MPLCustomSettings.currentFolder = setting.name;
+								MPLCustomSettings.UpdateSettingsPage();
+                            });
+
+                            EventTrigger trigger = setting.settingobj.AddComponent<EventTrigger>();
+                            trigger.triggers.Add(entry);
+                            trigger.enabled = false;
+                        }
+
+                        break;
+					case SettingType.Return:
+                        setting.settingobj = Object.Instantiate(navPanelButtons.transform.GetChild(0).gameObject, navPanelButtons.transform);
+                        setting.settingobj.SetActive(true);
+                        setting.control = setting.settingobj.transform.GetChild(0);
+                        setting.control.GetComponent<TextMeshProUGUI>().text = "Return?";
+                        setting.control.GetComponent<I2.Loc.Localize>().Term = "-";
+
+                        setting.control.GetComponent<TextMeshProUGUI>().fontStyle = FontStyles.Italic;
+
+                        Object.DestroyImmediate(setting.settingobj.GetComponent<Button>());
+						{
+                            EventTrigger.Entry entry = new EventTrigger.Entry();
+                            entry.eventID = EventTriggerType.PointerClick;
+                            entry.callback.AddListener((data) => {
+                                MPLCustomSettings.folderActive = false;
+                                MPLCustomSettings.UpdateSettingsPage();
+                            });
+
+                            EventTrigger trigger = setting.settingobj.AddComponent<EventTrigger>();
+                            trigger.triggers.Add(entry);
+                            trigger.enabled = false;
+                        }
+                        break;
+
 				}
-			}
+				if (setting.type != SettingType.Folder)
+				{
+					setting.control.GetComponent<TextMeshProUGUI>().alignment = TextAlignmentOptions.Left;
+				}
+            }
 
 			GameObject NextButton = Object.Instantiate(navPanelButtons.transform.GetChild(0).gameObject, navPanelButtons.transform);
 			NextButton.SetActive(true);
 			NextButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Next Page";
 			NextButton.transform.GetChild(0).GetComponent<I2.Loc.Localize>().Term = "Next Page";
-			Object.DestroyImmediate(NextButton.GetComponent<Button>());
+			NextButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().alignment = TextAlignmentOptions.Left;
+            Object.DestroyImmediate(NextButton.GetComponent<Button>());
 			{
 				EventTrigger.Entry entry = new EventTrigger.Entry();
 				entry.eventID = EventTriggerType.PointerClick;
@@ -289,7 +520,7 @@ class SettingsPatch
 			}
 			MPLCustomSettings.nextPage = NextButton.transform;
 
-			Object.DestroyImmediate(navPanelExit.GetComponent<Button>());
+            Object.DestroyImmediate(navPanelExit.GetComponent<Button>());
 			navPanelExit.gameObject.AddComponent<Button>();
 			navPanelExit.GetComponent<Button>().onClick.AddListener(() => { S.I.optCtrl.ClosePanel(modSettingsMenu.GetComponent<NavPanel>()); });
 			Traverse.Create(navPanelExit.GetComponent<UIButton>()).Field("button").SetValue(navPanelExit.GetComponent<Button>());
